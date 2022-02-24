f972d75ae881349c7f7f7a5a899362d51bf2ee9c
Jeff Fisher <jeffish@microsoft.com>
2022-02-24 15:47:08 -0800

    cleanup

--- src/tsEmitter.ts
+++ src/tsEmitter.ts
@@ -6,20 +6,14 @@ export interface TSEmitterOptions {
 
 export async function $onEmit(p: Program): Promise<void> {
   const options: TSEmitterOptions = {
-    outputPath: p.compilerOptions.outputPath || resolvePath("./ts"),
+    outputPath: p.compilerOptions.outputPath || resolvePath("./ts/"),
   };
 
-  const emitter = createTSEmitter(p, options);
-  await emitter.emitTS();
-}
-
-function createTSEmitter(p: Program, options: TSEmitterOptions) {
-  async function emitTS() {
-    const ns = getServiceNamespaceString(p);
-    console.log(ns);
-    console.log(options.outputPath);
+  const ns = getServiceNamespaceString(p);
+  console.log(ns);
+  console.log(options.outputPath);
+  if (!p.compilerOptions.noEmit) {
+    const outFile = resolvePath(options.outputPath, "client.ts");
+    await p.host.writeFile(outFile, "//hello world!");
   }
-  return {
-    emitTS,
-  };
 }
