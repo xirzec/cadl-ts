e79ffb323038b085c7799494283fa5f3095567eb
Jeff Fisher <jeffish@microsoft.com>
2022-03-22 17:51:27 -0700

    fix lint war in prettier

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   "plugins": ["@typescript-eslint"],
   "rules": {
-    "indent": ["error", 2],
+    "indent": ["error", 2, { "SwitchCase": 1 }],
     "linebreak-style": ["error", "windows"],
     "quotes": ["error", "double"],
     "semi": ["error", "always"]
